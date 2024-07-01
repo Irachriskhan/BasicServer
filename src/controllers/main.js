6221aba933b7e2bda3aba72332aee7adc63d0780
Christophe Irakoze <irachriskhan@gmail.com>
2024-07-02 01:45:22 +0200

    add region temperature

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -13,40 +13,28 @@ const mainController = async (req, res) => {
 
     if (!weatherAPI) return res.send({ Error: "No weather Info found" });
 
+    const userRegion = weatherAPI.region;
+
+    const url2 = `http://api.weatherapi.com/v1/current.json?key=00d736c6795342d48db183950240107&q=${userRegion}`;
+    const response2 = await fetch(url2);
+
+    if (!response2.ok) {
+      throw new Error("Failed to fetch temperature");
+    }
+
+    const temperatureAPI = await response2.json();
+
+    if (!temperatureAPI)
+      return res.send({ Error: "Failed to get the temperature data" });
+
     res.send({
       client_ip: weatherAPI.ip,
       location: weatherAPI.region,
-      greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${weatherAPI.city}`,
+      greeting: `Hello, ${visitor_name}!, the temperature is ${temperatureAPI.current.temp_c} degrees Celcius in ${weatherAPI.city}`,
     });
   } catch (error) {
-    console.log(error);
-    res.send({ error: error });
+    res.send({ error: error.message });
   }
 };
 
 module.exports = { mainController };
-
-// example.com>/api/hello?visitor_name="Mark"
-//   {
-//     "client_ip": "127.0.0.1", // The IP address of the requester
-//     "location": "New York" // The city of the requester
-//     "greeting": "Hello, Mark!, the temperature is 11 degrees Celcius in New York"
-//   }
-
-//   {
-//     "ip": "105.178.32.148",
-//     "type": "ipv4",
-//     "continent_code": "AF",
-//     "continent_name": "Africa",
-//     "country_code": "RW",
-//     "country_name": "Rwanda",
-//     "is_eu": "false",
-//     "geoname_id": 202061,
-//     "city": "Kigali (Remera)",
-//     "region": "Kigali",
-//     "lat": -1.95064,
-//     "lon": 30.1031,
-//     "tz_id": "Africa/Kigali",
-//     "localtime_epoch": 1719860062,
-//     "localtime": "2024-07-01 20:54"
-// }
